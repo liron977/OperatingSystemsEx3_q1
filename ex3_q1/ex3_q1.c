216b5f537afdfb0a82ed69de34bf0f5b22208e37
Liron aviv Kotev <lironkt@mta.ac.il>
2022-12-25 15:19:44 +0200

    new

--- ex3_q1/ex3_q1.c
+++ ex3_q1/ex3_q1.c
@@ -10,21 +10,29 @@ int done_threads = 0;
 int number_of_files = 0;
 
 int main(int agrc, char* argv[]) {
-    int num_stud = 0;
-    FILE* output_file = NULL;
-    char* input_file_name;
-    pthread_t threads[10];
+  
+    pthread_t threads[MAX_FILES];
 
-    number_of_files = agrc - 2;
-    for (int i = 2; i < agrc; i++) {
+    divide_work(agrc,threads,argv);
+ /*   number_of_files = agrc - 1;
+    for (int i = 1; i < agrc; i++) {
         input_file_name = argv[i]; 
-        pthread_create(&threads[i - 2], NULL, read_data_from_file, (void*)input_file_name);
-    }
+        pthread_create(&threads[i - 1], NULL, read_data_from_file, (void*)input_file_name);
+    }*/
     wait_for_threads(threads);
 
     pthread_exit(NULL);
 }
-void wait_for_threads(pthread_t threads[10]) {
+void divide_work(int agrc, pthread_t threads[MAX_FILES],char* argv[]) {
+    number_of_files = agrc - 1;
+    char* input_file_name;
+
+    for (int i = 1; i < agrc; i++) {
+        input_file_name = argv[i];
+        pthread_create(&threads[i - 1], NULL, read_data_from_file, (void*)input_file_name);
+    }
+}
+void wait_for_threads(pthread_t threads[MAX_FILES]) {
 
     for (int i = 0; i < number_of_files; i++) {
         pthread_join(threads[i], NULL);
@@ -33,8 +41,16 @@ void wait_for_threads(pthread_t threads[10]) {
          print_student(i);
      }*/
     fprintf(stderr, "all %d threads terminated\n", done_threads);
+    destroy();
 }
-void read_data_from_file(void* file_name_input) {
+void destroy() {
+    pthread_mutex_destroy(&all_stud_mtx);
+    pthread_mutex_destroy(&done_threads_mtx);
+    pthread_mutex_destroy(&cond_mtx);
+    pthread_cond_destroy(&count_done_threads);
+
+}
+void *read_data_from_file(void *file_name_input) {
     char* file_name = (char*)file_name_input;
     printf("%s file_name\n", file_name);
   
@@ -47,6 +63,7 @@ void read_data_from_file(void* file_name_input) {
 
     fclose(file);
     handle_done_threads();
+    pthread_exit(NULL);
   
 }
 void handle_done_threads() {
@@ -91,35 +108,10 @@ void get_student_average(char* line) {
     avg = (double)sum_of_grades / (counter);
     strcpy(stud.name ,student_name);
     stud.avg_grade = avg;
-  pthread_mutex_lock(&all_stud_mtx);
+    pthread_mutex_lock(&all_stud_mtx);
     add_to_student_arr(&stud);
-   // done_threads++;
     pthread_mutex_unlock(&all_stud_mtx);
-    /*
-    pthread_mutex_lock(&cond_mtx);
-    if (done_threads == number_of_files) {
-        printf("%d done_threads\n", done_threads);
-        pthread_cond_broadcast(&count_done_threads);
-        pthread_mutex_unlock(&cond_mtx);
-    }
-    else {
-        printf("waiting to wake % ld\n", pthread_self());
-        printf("%d done_threads\n", done_threads);
-     
-        pthread_cond_wait(&count_done_threads, &cond_mtx);
-        printf("I`m wake\n% ld\n", pthread_self());
-
-        pthread_mutex_unlock(&cond_mtx);
-    }*/
-  
-    
-
-
-
-
-
-   /* fprintf(stdout, "%s", student_name);
-    fprintf(stdout, " %.1f\n", avg);*/
+ 
 }
 
 
--- ex3_q1/ex3_q1.h
+++ ex3_q1/ex3_q1.h
@@ -7,12 +7,13 @@
 #define ex3_q1
 #define GRADES_FILE "all_std.txt"
 #define MAX_LINE_LENGTH 80
+#define MAX_FILES 10
 
 void get_student_average(char* line);
-void report_data_summary(int num_stud);
-FILE* open_output_file(char* open_mode);
-void read_data_from_file(void* file_name_input);
+void* read_data_from_file(void* file_name_input);
 void handle_done_threads();
 void wait_for_threads(pthread_t threads[10]);
+void destroy();
+void divide_work(int agrc, pthread_t threads[MAX_FILES], char* argv[]);
 
 #endif
